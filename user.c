8599b9341e0a228f83b6f6cbba62c0811660dc40
hialvaro <github@r3d.red>
2021-09-20 18:52:09 +0200

    Adds add user function

--- user.c
+++ user.c
@@ -4,12 +4,18 @@ char buff[24];
 
 int pid;
 
+int add(int p1, int p2){
+	return p1+p2;
+}
+
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-    
-  while(1) { }
+  
+  while(1) { 
+  	int val1 = add(0x42, 0x666);
+  }
 }

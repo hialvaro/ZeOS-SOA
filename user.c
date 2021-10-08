cd41b5cbc8d0ab329b63cb517b85bc1e88f81678
alvaro.moreno.ribot <alvaro.moreno.ribot@estudiantat.upc.edu>
2021-10-08 22:42:33 +0200

    Adds call to gettime from user space

--- user.c
+++ user.c
@@ -18,6 +18,7 @@ int __attribute__ ((__section__(".text.main")))
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
   write(1, "\nTest write from user.c\n", 24);
+  gettime();
   while(1) { 
   	//int val1 = add(0x42, 0x666);
   	//int val2 = addASM(0x42, 0x666);

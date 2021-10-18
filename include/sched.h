36765a9022066fb6a524243271f647aec915f897
alvaro.moreno.ribot <alvaro.moreno.ribot@estudiantat.upc.edu>
2021-10-18 13:48:35 +0200

    Adds the freequeue and its initialization

--- include/sched.h
+++ include/sched.h
@@ -16,6 +16,7 @@ enum state_t { ST_RUN, ST_READY, ST_BLOCKED };
 
 struct task_struct {
   int PID;			/* Process ID. This MUST be the first field of the struct. */
+  struct list_head list;
   page_table_entry * dir_pages_baseAddr;
 };
 
--- sched.c
+++ sched.c
@@ -9,7 +9,7 @@
 union task_union task[NR_TASKS]
   __attribute__((__section__(".data.task")));
 
-#if 0
+#if 1
 struct task_struct *list_head_to_task_struct(struct list_head *l)
 {
   return list_entry( l, struct task_struct, list);
@@ -17,6 +17,7 @@ struct task_struct *list_head_to_task_struct(struct list_head *l)
 #endif
 
 extern struct list_head blocked;
+struct list_head freequeue;
 
 
 /* get_DIR - Returns the Page Directory address for task 't' */
@@ -65,7 +66,11 @@ void init_task1(void)
 
 void init_sched()
 {
-
+	INIT_LIST_HEAD( &freequeue );
+	int i;
+	for(i = 0; i<NR_TASKS;i++){
+		list_add( &(task[i].task.list), &freequeue);
+	}
 }
 
 struct task_struct* current()

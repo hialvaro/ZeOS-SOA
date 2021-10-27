8c2c0f722e74517e3580682d712dd137fe3c1bb0
hialvaro <github@r3d.red>
2021-10-27 09:54:50 +0200

    Remove monoprocess_init_addr_space

--- system.c
+++ system.c
@@ -83,7 +83,7 @@ int __attribute__((__section__(".text.main")))
   init_mm();
 
   /* Initialize an address space to be used for the monoprocess version of ZeOS */
-  monoprocess_init_addr_space(); /* TO BE DELETED WHEN THE PROCESS MANAGEMENT CODE TO BECOME MULTIPROCESS IS ADDED */
+  //monoprocess_init_addr_space(); /* TO BE DELETED WHEN THE PROCESS MANAGEMENT CODE TO BECOME MULTIPROCESS IS ADDED */
 
   /* Initialize Scheduling */
   init_sched();

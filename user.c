36fe088a3d5ce3108fc34d566b7507cc22c1bfee
hialvaro <github@r3d.red>
2021-11-05 00:58:04 +0100

    Add tests for sys_fork and sys_exit

--- user.c
+++ user.c
@@ -19,7 +19,7 @@ int __attribute__ ((__section__(".text.main")))
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
   
   /********WRITE********/
-  if(write(1, "------- \n", 9) == -1) perror();
+  if(write(1, "\n------- \n", 9) == -1) perror();
   char * mesg;
   mesg = "\n [write] Write from user.c\n";
   if(write(1, mesg, strlen(mesg)) == -1) perror();
@@ -42,17 +42,43 @@ int __attribute__ ((__section__(".text.main")))
   int pid = getpid();
   itoa(pid, mesg);
   if(write(1, mesg, strlen(mesg)) == -1) perror();
-  mesg = "------- \n\n";
+  mesg = "\n------- \n\n";
   if(write(1, mesg, strlen(mesg)) == -1) perror();
 
-  /*********FORK**********/
+  /*********FORK*********
   mesg = "[fork] Forking... \n CHIDL PID: ";
   if(write(1, mesg, strlen(mesg)) == -1) perror();
   int pid_child = fork();
   itoa(pid_child, mesg);
   if(write(1, mesg, strlen(mesg)) == -1) perror();
   mesg = "\n[fork] Job is done. \n ------- \n\n";
-  if(write(1, mesg, strlen(mesg)) == -1) perror();
+  if(write(1, mesg, strlen(mesg)) == -1) perror();*/
+
+  /*****FORK TRUE TEST***/
+  int child = fork();
+  if(child == 0){
+    mesg="I am the CHILD and my PID is ";
+    if(write(1, mesg, strlen(mesg)) == -1) perror();
+    itoa(getpid(), mesg);
+    if(write(1, mesg, strlen(mesg)) == -1) perror();
+    mesg="\n";
+    if(write(1, mesg, strlen(mesg)) == -1) perror();
+    exit();
+    mesg="Child exited with PID ";
+    if(write(1, mesg, strlen(mesg)) == -1) perror();
+    itoa(getpid(), mesg);
+    if(write(1, mesg, strlen(mesg)) == -1) perror();
+    mesg="\n";
+    if(write(1, mesg, strlen(mesg)) == -1) perror();
+  }
+  else{
+    mesg="I am the FATHER and my PID is ";
+    if(write(1, mesg, strlen(mesg)) == -1) perror();
+    itoa(getpid(), mesg);
+    if(write(1, mesg, strlen(mesg)) == -1) perror();
+    mesg="\n";
+    if(write(1, mesg, strlen(mesg)) == -1) perror();
+  }
 
   while(1) { 
   	//int val1 = add(0x42, 0x666);

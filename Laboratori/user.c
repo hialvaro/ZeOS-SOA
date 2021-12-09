c6cabb4589ea873e2ef3ed8ef7c3532948663361
hialvaro <github@r3d.red>
2021-12-09 19:55:58 +0100

    Update semaphores tests

--- Laboratori/user.c
+++ Laboratori/user.c
@@ -16,24 +16,53 @@ int __attribute__ ((__section__(".text.main")))
   write(1, mesg, strlen(mesg));
 
   // Invalid semaphore: negative id.
-  if (sem_init(-3,2) == 0) write(1, "\nT", 2);
-  else write(1, "\nInvalid", strlen("\nInvalid"));
+  mesg = "\n\nTrying to create a SEMAPHORE with ID=-3";
+  write(1, mesg, strlen(mesg));
 
-  if (sem_init(3,2) == 0) write(1, "\nT", 2);
-  else write(1, "\nInvalid", strlen("\nInvalid"));
+  if (sem_init(-3,2) == 0)   mesg = "\nValid ID -3";
+  else mesg = "\nInvalid ID -3";
+  write(1, mesg, strlen(mesg));
 
-  // Invalid semaphore: repeated id.
-  if (sem_init(3,2) == 0) write(1, "\nT", 2);
-  else write(1, "\nInvalid", strlen("\nInvalid"));
+  mesg = "\n\nTrying to destroy a SEMAPHORE with ID=-3";
+  write(1, mesg, strlen(mesg));
+  if (sem_destroy(-3) == 0)   mesg = "\nDestroy OK";
+  else mesg = "\nDestroy Invalid";
+  write(1, mesg, strlen(mesg));
 
-  if (sem_init(1,2) == 0) write(1, "\nT", 2);
-  else write(1, "\nInvalid", strlen("\nInvalid"));
+  mesg = "\n\nTrying to create a SEMAPHORE with ID=4";
+  write(1, mesg, strlen(mesg));
+  if (sem_init(4,2) == 0)   mesg = "\nValid ID 4";
+  else mesg = "\nInvalid ID 4";
+  write(1, mesg, strlen(mesg));
 
-  if (sem_init(5,1) == 0) write(1, "\nSemaphore created", strlen("\nSemaphore created"));
-  else write(1, "\nInvalid", strlen("\nInvalid"));
+  mesg = "\n\nTrying to REcreate a SEMAPHORE with ID=4";
+  write(1, mesg, strlen(mesg));
+  if (sem_init(4,2) == 0)   mesg = "\nValid ID 4";
+  else mesg = "\nInvalid ID 4: REPEATED";
+  write(1, mesg, strlen(mesg));
 
+  mesg = "\n\nTrying to destroy the SEMAPHORE with ID=4";
+  write(1, mesg, strlen(mesg));
+  if (sem_destroy(4) == 0)   mesg = "\nDestroy OK";
+  else mesg = "\nDestroy Invalid";
+  write(1, mesg, strlen(mesg));
+
+  mesg = "\n\nTrying to REcreate the SEMAPHORE with ID=4 after destroy";
+  write(1, mesg, strlen(mesg));
+  if (sem_init(4,2) == 0)   mesg = "\nValid ID 4";
+  else mesg = "\nInvalid ID 4";
+  write(1, mesg, strlen(mesg));
+
+  mesg = "\n\nCreating a SEMAPHORE with ID=5 to test";
+  write(1, mesg, strlen(mesg));
+  if (sem_init(5,1) == 0)   mesg = "\nValid ID 5";
+  else mesg = "\nInvalid ID 5";
+  write(1, mesg, strlen(mesg));
+
+  mesg = "\n\nsem_wait and sem_signal test";
+  write(1, mesg, strlen(mesg));
   pid = fork();
-  if(sem_wait(5) == 0) write(1, "\nWait OK", strlen("\nWait OK"));
+  if(sem_wait(5) == 0) write(1, "\nWait success", strlen("\nWait success"));
   if(pid > 0){
     write(1, "\nfather says", strlen("\nfather says"));
     mesg = "\nhi";
@@ -44,6 +73,5 @@ int __attribute__ ((__section__(".text.main")))
   }  
   write(1, mesg, strlen(mesg));
   sem_signal(5);
-
   while(1){}
 }
